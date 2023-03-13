61f35ae1199a41786dedcb589618e2b73bb0a4c2
umidjon_thinkland <umidjon@thinkland.uz>
2023-03-13 13:18:25 +0500

    initial commit

--- test/soft_cli_test.dart
+++ test/soft_cli_test.dart
@@ -1,12 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:soft_cli/soft_cli.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
 }
